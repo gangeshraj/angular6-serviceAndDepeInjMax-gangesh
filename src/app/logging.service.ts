daa62c94c5eb9ea9e672af3ba8bd3ef7e18743c5
gangeshraj <gangeshraj1@gmail.com>
2018-05-23 08:55:16 +0530

    added services

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,5 +1,5 @@
-export class LoggingService{
-    logStatusChange(status:string){
-        
-    }
+export class LoggingService {
+  logStatusChange(status: string) {
+    console.log('A server status changed, new status: ' + status);
+  }
 }
